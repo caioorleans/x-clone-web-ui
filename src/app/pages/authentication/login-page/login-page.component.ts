a2234c2776c581a2cbf7f4536e893460a8691f01
Caio Orleans <caioorleansdm@gmail.com>
2024-04-09 14:29:14 -0300

    - Extracted logo to a component

--- src/app/pages/authentication/login-page/login-page.component.ts
+++ src/app/pages/authentication/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../../../components/logo/logo.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    LogoComponent
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
