9d4ebc8aea194f8d89875de7f7400fe00c7ebe75
Caio Orleans <caioorleansdm@gmail.com>
2024-04-09 12:10:00 -0300

    - Dark mode implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors:{
+      'xblue': '#1D9BF0',
+      'black-font': '#0F1419',
+      'white-font': '#E7E9EA',
+      'light-gray-font': '#71767B',
+      'dark-gray-font': '#536471',
+      'xlight-gray-font':'#EFF3F4',
+      'xdark-gray-font':'#2F3336',
+      'gray-border': '#DADCE0',
+      'btn-hover': '#E6E6E6',
+      'blue-btn-hover': '#1B8CD8',
+      'trs-btn-hover': '#E8F5FD',
+      'trs-btn-dark-hover':'#031018',
+      'black': '#000000',
+      'white': '#fff'
+    },
     fontFamily: {
       'chirp':['chirp']
     },
