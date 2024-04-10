4cc9163fc795d0cf854f481705a10c8849079478
Caio Orleans <caioorleansdm@gmail.com>
2024-04-10 18:32:35 -0300

    - Installed Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
@@ -25,6 +26,8 @@ module.exports = {
       'chirp':['chirp']
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
