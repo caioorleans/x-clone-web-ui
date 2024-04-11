a728536e7e2e7040b296949d27b6a529dd8b5309
Caio Orleans <caioorleansdm@gmail.com>
2024-04-11 16:24:49 -0300

    - Button font now bold

--- src/app/components/buttons/blue-button/blue-button.component.ts
+++ src/app/components/buttons/blue-button/blue-button.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'app-blue-button',
   standalone: false,
   template: `
-    <button type="button" class="w-full text-white bg-xblue hover:bg-blue-btn-hover focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-blue-800">
+    <button type="button" class="w-full text-white bg-xblue hover:bg-blue-btn-hover focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-blue-800">
       {{text}}
     </button>
   `,
--- src/app/components/buttons/transparent-button/transparent-button.component.ts
+++ src/app/components/buttons/transparent-button/transparent-button.component.ts
@@ -4,7 +4,7 @@ import { Component, Input} from '@angular/core';
   selector: 'app-transparent-button',
   standalone: false,
   template: `
-    <button type="button" class="mt-4 w-full text-xblue bg-transparent border border-gray-border focus:outline-none hover:bg-trs-btn-hover dark:hover:bg-trs-btn-dark-hover focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2">
+    <button type="button" class="mt-4 w-full text-xblue bg-transparent border border-gray-border focus:outline-none hover:bg-trs-btn-hover dark:hover:bg-trs-btn-dark-hover focus:ring-4 focus:ring-gray-100 font-bold rounded-full text-sm px-5 py-2.5 me-2 mb-2">
       {{text}}
     </button>
   `
